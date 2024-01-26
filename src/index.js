a4c6ab9d3aed044c02e876dc9678a5e4b426de6a
Mazora <64915756+MazoraMelon@users.noreply.github.com>
2024-01-26 19:02:43 +0000

    Command Access

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 require('dotenv').config();
+const fs = require('fs'); 
 const { Client, Events, GatewayIntentBits, ButtonBuilder, ButtonStyle, SlashCommandBuilder, ActionRowBuilder, permissionOverwrites, PermissionsBitField } = require('discord.js');
+
+logToFile("Bot Started up!")
+//SUPABASE--------------------------------------------------------------------------------
+const { createClient } = require('@supabase/supabase-js');
+const { log } = require('console');
+const supabaseUrl = 'https://rvydqlglkydpzfqhodsl.supabase.co'
+const supabaseKey = process.env.SUPABASE_KEY
+const supabase = createClient(supabaseUrl, supabaseKey)
+
+
 
 const client = new Client({
 	 
@@ -27,18 +38,40 @@ function generateToken() {
   }
   
 
+async function createOrderSupabase(channel_id, customer) {
+    var price = 0
+    var currency = "⏣"
+    var priority = 3
+    var paid = false
+    var company = "acf97810-b59e-4fb1-a434-a72484b9b9d0"
+    var devs = ""
+    var details = "Details not added yet"
+    var product = "-----"
 
+    // Add the order to supabase
+    const insert = await supabase
+        .from('orders')
+        .insert({ product, customer, price, currency, priority, paid, company, devs, details, channel_id })
+        .select();
+
+    if (insert.error) {
+        console.error('Supabase Insert Error:', insert.error.message);
+        logToFile(`${customer} failed to create an order due to the error: ${insert.error.message}`)
+    } else {
+        console.log('Supabase Insert Success:', insert.data);
+        logToFile(`${customer} created the order #${insert.data[0].id} \t Channel ID: ${channel_id} `)
+    }
+    
+}
 
 client.once(Events.ClientReady, readyClient => {
 	console.log(`${readyClient.user.tag} is online!`);
 });
-
 client.on("interactionCreate", interaction => {
 	if(!interaction.isChatInputCommand()) return;
 
 	switch(interaction.commandName) {
 		case "order":
-
 			const create = new ButtonBuilder()
 			.setCustomId('create')
 			.setLabel('Create An Order')
@@ -49,7 +82,7 @@ client.on("interactionCreate", interaction => {
 			.addComponents(create);
 
 			interaction.reply({content: `Create an order`, components: [row]});
-			
+			logToFile(`${interaction.user.username} used the create order command`)
 			break;
 
 	}
@@ -81,8 +114,19 @@ client.on("interactionCreate", async (interaction) => {
 
             await interaction.reply({ content: `Thanks! Your order channel is ready! ${channel}`, ephemeral: true });
 
+            // Add order to file
+            addOrderToFile(channel.id, interaction.user.username, interaction.user.id);
+
+
             // Send a message in the new order chat
             await channel.send({ content: `Hi there! ${interaction.user}. Thanks for making an order! Just so you know, I store data about this order on my database!` });
+
+            // Add the user order to supabase
+            let channelID = channel.id;
+            let user = interaction.user.username;
+            createOrderSupabase(channelID, user)
+
+            // After 3 seconds delete the channel
 
             break;
             
@@ -94,22 +138,38 @@ client.on("interactionCreate", async (interaction) => {
     if (interaction.isCommand()) {
         const { commandName } = interaction;
 
+        
+
         switch (commandName) {
             case "close":
-                const channel = await interaction.guild.channels.cache.find(channel => channel.name === `order-${interaction.user.username}`);
-                await interaction.user.send("Your order has been closed! Thanks for working with us!");
-                await channel.setParent(interaction.guild.channels.cache.find(channel => channel.name === "archive"));
+                var userid = await findUserByChannelId(interaction.channelId);
+                // Perms
+                if (!interaction.member.permissions.has(PermissionsBitField.Flags.ManageChannels)) {
+                    await interaction.reply("You do not have permission to use this command", { ephemeral: true });
+                    break;
+                }
 
+                if (!userid) {
+                    await interaction.reply("Command used in an invalid channel", { ephemeral: true });
+                    break;
+                }
+                userid = await client.users.fetch(userid);
+                const channel = await interaction.guild.channels.fetch(interaction.channelId);
+                await interaction.reply("This order has been closed!")
                 await channel.permissionOverwrites.set([
                     {
                         id: interaction.guild.roles.everyone.id,
                         deny: [PermissionsBitField.Flags.SendMessages, PermissionsBitField.Flags.ViewChannel],
                     }, {
-                        id: interaction.user.id,
+                        id: userid.id,
                         deny: [PermissionsBitField.Flags.SendMessages, PermissionsBitField.Flags.ViewChannel],
                     },
+                    
                 ]);
+                await userid.send("Your order has been closed! Thanks for working with us!");
+                channel.setParent(interaction.guild.channels.cache.find(channel => channel.name === "archive"));
                 break;
+
 
             default:
                 break;
@@ -120,15 +180,6 @@ client.on("interactionCreate", async (interaction) => {
 
 
 
-
-//SUPABASE--------------------------------------------------------------------------------
-
-const { createClient } = require('@supabase/supabase-js');
-const supabaseUrl = 'https://rvydqlglkydpzfqhodsl.supabase.co'
-const supabaseKey = process.env.SUPABASE_KEY
-const supabase = createClient(supabaseUrl, supabaseKey)
-
-
 //CHARGE CUSTOMER-----------------------------------------------------------------
 client.on("interactionCreate", async (interaction) => {
     if (interaction.isCommand()) {
@@ -136,14 +187,26 @@ client.on("interactionCreate", async (interaction) => {
 
         switch (commandName) {
             case "charge":
+                // Check the user roles
+                if (!interaction.member.roles.cache.some(role => role.name === "Founder")) {
+                    await interaction.reply("You do not have permission to use this command.", { ephemeral: true });
+                    break;
+                }
+            // Check the channels user using src/storage/orders.json
+            var userid = await findUserByChannelId(interaction.channelId);
 
-                let user = "mazora_";
-
+            if (userid === null) {
+                await interaction.reply("This command was used in an invalid channel.", { ephemeral: true });
+                console.log("Null")
+                break;
+            }
+            userid = await client.users.fetch(userid);
+            username = userid.username;
                 // Check if the user already exists in the 'customers' table
                 const existingUserData = await supabase
                     .from('customers')
                     .select('*')
-                    .eq('discordid', user);
+                    .eq('discordid', username);
 
                 if (existingUserData.error) {
                     console.error('Supabase Select Error:', existingUserData.error.message);
@@ -155,11 +218,13 @@ client.on("interactionCreate", async (interaction) => {
                     console.log('User already exists in the table:', existingUserData.data);
                     console.log(existingUserData.data[0].token);
                     let token = existingUserData.data[0].token;
+
+                    console.log(userid.username)
                     // Check if the first letter of the token is an underscore
                     if (token.startsWith('_')) {
 
                         token = token.slice(1); // Remove the underscore
-                        await interaction.user.send(`Put this token into the payment to pay! ||${token}|| https://www.roblox.com/games/16078251436/Admire-Pay`);
+                        await userid.send(`Put this token into the payment to pay! ||${token}|| https://www.roblox.com/games/16078251436/Admire-Pay`);
                         await interaction.reply(` The user has recieved their payment token, please do not share it! :)`);
                     } else {
                         await interaction.reply(`Your account has been linked so this should be easy! Enter this game as: **${token}**! https://www.roblox.com/games/16078251436/Admire-Pay`);
@@ -168,13 +233,13 @@ client.on("interactionCreate", async (interaction) => {
                     // User doesn't exist, insert the new data
 
                     let token = generateToken();
-                    await interaction.user.send(`Your account token is ||${token}|| put this into the game to pay! https://www.roblox.com/games/16078251436/Admire-Pay`);
+                    await userid.send(`Your account token is ||${token}|| put this into the game to pay! https://www.roblox.com/games/16078251436/Admire-Pay`);
                     await interaction.reply(` The customer for this order has recieved a DM of their token, please do not share it! :) https://www.roblox.com/games/16078251436/Admire-Pay`);
                     
 
                     const insert = await supabase
                         .from('customers')
-                        .insert([{ discordid: user, token: ("_" + token) }])
+                        .insert([{ discordid: username, token: ("_" + token) }])
                         .select();
 
                     if (insert.error) {
@@ -188,6 +253,54 @@ client.on("interactionCreate", async (interaction) => {
     }
 });
 
+
+function logToFile(content) {
+    // Get the current date and time
+    const timestamp = new Date().toISOString();
+  
+    // Format the log entry
+    const logEntry = `${timestamp} - ${content}\n`;
+  
+    // Append the log entry to the log.txt file
+    fs.appendFile('src/storage/log.txt', logEntry, (err) => {
+      if (err) {
+        console.error('Error writing to log file:', err);
+      }
+    });
+  }
+  
+function readOrdersFromFile() {
+    try {
+        const data = fs.readFileSync('src/storage/orders.json', 'utf8');
+        return JSON.parse(data);
+    } catch (err) {
+        console.error('Error reading orders file:', err);
+        return [];
+    }
+}
+function writeOrdersToFile(orders) {
+    fs.writeFile('src/storage/orders.json', JSON.stringify(orders, null, 3), 'utf8', (err) => {
+        if (err) {
+            console.error('Error writing orders file:', err);
+        } else {
+            console.log('Orders written to file successfully.');
+        }
+    });
+}
+function addOrderToFile(channel_id, user, user_id) {
+    const orders = readOrdersFromFile();
+
+    const newOrder = { channel_id, user, user_id };
+    orders.push(newOrder);
+
+    writeOrdersToFile(orders);
+}
+
+function findUserByChannelId(channel_id) {
+    const orders = readOrdersFromFile();
+    const order = orders.find(order => order.channel_id === channel_id);
+    return order ? order.user_id : null;
+}
 
 
 client.login(process.env.TOKEN);
